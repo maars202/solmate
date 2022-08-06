96655ac1ce1c3ea1d1efd05172542f12571ab353
maars202 <85531823+maars202@users.noreply.github.com>
2022-08-06 16:35:53 +0800

    copy

--- frontend/copyidl.js
+++ frontend/copyidl.js
@@ -1,5 +1,6 @@
 // copyIdl.js
 const fs = require('fs');
-const idl = require('../mysolanaapp/target/idl/mysolanaapp.json');
+// const idl = require('../mysolanaapp/target/idl/mysolanaapp.json');
+const idl = require('../target/idl/mysolanaapp.json');
 
 fs.writeFileSync('./src/idl.json', JSON.stringify(idl));
