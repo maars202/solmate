87c7773fdf1998c77f1e6bed3d5d3a6fc75e095d
Liam Wells Ayathan <ayathan21@gmail.com>
2022-08-10 20:25:57 +0800

    added NFT_portfolio page plus reorganized

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './pages/home.js';
+import NFT_portfolio from './pages/NFT_portfolio';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <NFT_portfolio />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
